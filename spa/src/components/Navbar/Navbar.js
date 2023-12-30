44235bb5dd42220af98badded9fbecf295c65ed2
mahajankapil005 <130069883+mahajankapil005@users.noreply.github.com>
2023-12-30 14:33:42 +0000

    FIRST DONE

---
+++ spa/src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import "./Navbar.css"
+
+const Navbar = ()=>{
+    const design = (
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+        </ul>
+    );
+
+    return design;
+}
+
+export default Navbar;
